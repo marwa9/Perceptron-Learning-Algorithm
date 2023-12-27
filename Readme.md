bd26d90a3c670dfa3b17a5272bca521fe9b7f221
Marwa <kchaou.marwa94@gmail.com>
2023-12-27 09:44:22 +0100

    add codes

---
+++ Readme.md
@@ -0,0 +1,20 @@
+This repo contains the code base for the Perceptron Learning Algorithm.
+
+The problem details can be found [here](https://baylor.kattis.com/courses/CSI5325/20s/assignments/kwne7w/problems/baylor.perceptron).
+
+The algorithm is very sensitive to the random seed selected.
+The weights that should give accuracy = 1 are w = [-149.0,2.0,2.0].
+
+I set the random seed to 0 and obtained the following results: 
+learned weights = [-222.0,2.0,4.0]
+training accuracy = 1
+testing accuracy = 0.8
+
+
+Below, a 2D visualization of the learned decision boundary and test data:
+
+<p align="center">
+<img src="images/2D_Visulization_test_data_decision_boundary.png" width="400" height="300">
+</p>
+
+**I used two libraries: numpy and matplotlib for visualization.**
---
+++ main.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 26 17:11:02 2023
+
+@author: Marwa Kechaou
+
+Solution to the problem:
+https://baylor.kattis.com/courses/CSI5325/20s/assignments/kwne7w/problems/baylor.perceptron
+"""
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+# Set the random seed for reproducibility
+np.random.seed(0)
+
+# Define training and testing data
+X_train = np.array([[77,66],[86,14],[50,21],[42,82],[28,78],[22,32]])
+y_train = np.array([1,1,-1,1,1,-1])
+
+X_test = np.array([[23,47],[26,2],[96,78],[1,65],[64,40],[13,68],
+                   [24,5],[69,32],[21,19],[65,88]])
+y_test = np.array([-1,-1,1,-1,1,1,-1,1,-1,1])
+
+# Implement the Perceptron Learning Algorithm
+def PLA(X, y, max_iter=1000, learning_rate=1):
+    """
+    Parameters:
+    - X: Input features
+    - y: labels
+    - max_iter: Maximum number of iterations (default is set to 1000). 
+    The stopping condition could be that there are no more misclassified points, 
+    indicating that the algorithm has converged and found a solution. 
+    The max_iter parameter is a safety measure to avoid infinite loops in case the 
+    data is not linearly separable.
+
+    Returns:
+    - w: Learned weights (numpy array).
+    """
+    X = np.column_stack((np.ones(X.shape[0]), X))
+    w = np.zeros(X.shape[1])
+
+    for i in range(max_iter):
+        misclassified_points = []
+        
+        for j in range(X.shape[0]):
+            if np.sign(np.dot(w, X[j, :])) != y[j]:
+                misclassified_points.append(j)
+
+        if not misclassified_points:
+            break
+
+        random_misclassified_point = np.random.choice(misclassified_points)
+
+        # Update weights with a learning rate
+        w = w + learning_rate * y[random_misclassified_point] * X[random_misclassified_point, :]
+    return w
+
+def Perceptron_evaluation(w, X_test, y_test):
+    # Add a column for the bias term (w0) to the test features
+    X_test = np.column_stack((np.ones(X_test.shape[0]), X_test))
+
+    predictions = np.sign(np.dot(X_test, w))
+    accuracy = np.mean(predictions == y_test)
+    return accuracy
+    
+def decision_boundary(X_test, y_test, w):
+    # Plot test data points
+    plt.scatter(X_test[:, 0][y_test==-1], X_test[:, 1][y_test==-1], c=y_test[y_test==-1], cmap=plt.cm.Paired, marker='s', edgecolors='k', label='Negative Test Data')
+    plt.scatter(X_test[:, 0][y_test==1], X_test[:, 1][y_test==1], c=y_test[y_test==1], cmap=plt.cm.Paired, marker='o', edgecolors='r', label='Positive Test Data')
+
+    # Plot decision boundary
+    x_min, x_max = np.min(X_test[:, 0]) - 1, np.max(X_test[:, 0]) + 1
+    y_min, y_max = np.min(X_test[:, 1]) - 1, np.max(X_test[:, 1]) + 1
+
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01),
+                         np.arange(y_min, y_max, 0.01))
+
+    Z = np.dot(np.c_[np.ones(xx.ravel().shape[0]), xx.ravel(), yy.ravel()], w)
+    Z = Z.reshape(xx.shape)
+
+    plt.contour(xx, yy, Z, colors='k', linewidths=2, levels=[0])
+    plt.title('Perceptron Decision Boundary')
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
+    plt.show()
+
+def main():
+    # Apply Perceptron Learning Algorithm
+    w = PLA(X_train, y_train)
+    # Print and plot results
+    print("Learned weights :"," ".join(map(str, w)))
+    print("training accuracy : ",Perceptron_evaluation(w,X_train, y_train))
+    print("testing accuracy : ",Perceptron_evaluation(w,X_test, y_test))
+    decision_boundary(X_test, y_test, w)
+
+if __name__ == "__main__":
+    main()
